8b648279ceed19f97953031218e1e6695cdfd695
megana <meghanadumpeti17@gmail.com>
2022-12-28 19:20:38 +0530

    1

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CartPageComponent } from './components/pages/cart-page/cart-page.component';
+import { FoodPageComponent } from './components/pages/food-page/food-page.component';
+import { HomeComponent } from './components/pages/home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'search/:searchTerm', component: HomeComponent },
+  { path: 'tag/:tag', component: HomeComponent },
+  {path:'food/:id', component:FoodPageComponent},
+  {path:'cart-page', component: CartPageComponent},
+  {path:'login',component:LoginComponent},
+  {path:'sign-up',component: RegisterComponent},
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
